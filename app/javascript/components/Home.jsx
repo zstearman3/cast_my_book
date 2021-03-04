8011f449ff2e4a2604c8581e298f803666e11010
Zach Stearman <zstearman3@gmail.com>
2021-03-04 20:30:55 +0000

    Add basic search functino

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import SearchBar from "./common/SearchBar"
 
 const Home = (props) => {
+  const books = props.books;
   return(
     <div className="page-container">
       <div className="home-search col-md-8 offset-md-2">
         <h2>Search for a Book</h2>
-        <SearchBar/>
+        <SearchBar
+          items={books}
+        />
         <p>Can't find what you want?<a href=""> Make a new one!</a></p>
       </div>
     </div>
--- app/javascript/components/common/SearchBar.jsx
+++ app/javascript/components/common/SearchBar.jsx
@@ -1,10 +1,24 @@
 import React from "react"
 
 const SearchBar = (props) => {
+  const [input, setInput] = React.useState(null);
+  const [filteredItems, setFilteredItems] = React.useState(null);
+  const items = props.items;
+  const handleInputChange = (search) => {
+    const newInput = search.toLowerCase()
+    setInput(newInput);
+    let itemList = items.filter(
+      (member) => {
+        return member.title.toLowerCase().indexOf(newInput) !== -1
+      }
+    )
+    setFilteredItems(itemList);
+  } 
   return (
     <div className="main-search">
       <input className="search-bar"
         placeholder={"Search Books"}
+        onChange={() => handleInputChange(event.target.value)}
       />
     </div>
   );
